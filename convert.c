3a50fdcc69a0a135f7decd6cc3d19681e3e5c073
qortpdms2 <128915671+qortpdms2@users.noreply.github.com>
2023-04-09 01:45:43 +0900

    Update convert.c

--- convert.c
+++ convert.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    int n;
+    int n, result = 0;
     scanf("%x", &n);
-    printf(n);
+    
+    for(int i=32768; i>=4096; i--){
+        result = (n & i);
+        if(result != 0) result = 1;
+        printf(result);
+    }
+    
+    for(int i=2048; i>=512; i--){
+        result = (n & i);
+        if(result != 0) result = 1;
+        printf(result);
+    }
+    
+    for(int i=256; i>=0; i--){
+        result = (n & i);
+        if(result != 0) result = 1;
+        printf(result);
+    }
+    
 
     return 0;
 }
